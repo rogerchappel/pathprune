5b139e1570974ef6aaf820d552d1d8c1514a28e0
Roger Chappel <miscanalysis@gmail.com>
2026-05-13 18:48:05 +1000

    feat: expose public library api

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { scan, exitCodeForReport, orderFindings } from './scanner.js';
+export { defaultConfig, mergeConfig, createConfigJson } from './config.js';
+export { formatJson, formatText } from './format.js';
+export type { FileEntry, Finding, FindingKind, OutputFormat, PathPruneConfig, ScanOptions, ScanReport, ScanSummary, Severity } from './types.js';
