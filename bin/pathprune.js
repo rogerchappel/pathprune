eeacf2874c6174d52bff51d339b4c1548a2abdeb
Roger Chappel <miscanalysis@gmail.com>
2026-05-13 18:57:02 +1000

    fix: point package entrypoints at built sources

--- bin/pathprune.js
+++ bin/pathprune.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { runCli } from '../dist/cli.js';
+import { runCli } from '../dist/src/cli.js';
 
 runCli(process.argv).catch((error) => {
   console.error(error instanceof Error ? error.message : String(error));
